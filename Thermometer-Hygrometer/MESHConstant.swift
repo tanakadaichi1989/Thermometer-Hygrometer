3b139938aae04cc957e9118f436f1d0d759627dd
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-12-17 20:23:35 +0900

    Add MESHConstant.swift

---
+++ Thermometer-Hygrometer/MESHConstant.swift
@@ -0,0 +1,15 @@
+//
+//  MESHConstant.swift
+//  Thermometer-Hygrometer
+//
+//  Created by 田中大地 on 2022/12/17.
+//
+
+import Foundation
+
+enum MESH: String {
+    case UUID = "72C90001-57A9-4D40-B746-534E22EC9F9E"
+    var description: String {
+        return rawValue
+    }
+}
